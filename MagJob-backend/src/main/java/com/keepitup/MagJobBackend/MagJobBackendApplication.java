4ac28c0aeec9e836eb4a2bd095cf17e0a1ce5fdc
WallyS02 <sebkutny@gmail.com>
2023-10-31 14:03:41 +0100

    MagJob backend initial commit

---
+++ MagJob-backend/src/main/java/com/keepitup/MagJobBackend/MagJobBackendApplication.java
@@ -0,0 +1,13 @@
+package com.keepitup.MagJobBackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MagJobBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MagJobBackendApplication.class, args);
+	}
+
+}
