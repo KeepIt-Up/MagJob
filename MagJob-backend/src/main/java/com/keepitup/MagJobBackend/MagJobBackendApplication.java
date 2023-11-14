3aeb399ce582df868f5fb4c196e0d56c67feab55
WallyS02 <sebkutny@gmail.com>
2023-11-14 16:45:49 +0100

    Excluding for convenience Spring Security auto-auth

--- MagJob-backend/src/main/java/com/keepitup/MagJobBackend/MagJobBackendApplication.java
+++ MagJob-backend/src/main/java/com/keepitup/MagJobBackend/MagJobBackendApplication.java
@@ -2,8 +2,9 @@ package com.keepitup.MagJobBackend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude= {SecurityAutoConfiguration.class})
 public class MagJobBackendApplication {
 
 	public static void main(String[] args) {
