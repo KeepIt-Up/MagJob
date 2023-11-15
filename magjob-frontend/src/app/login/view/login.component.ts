a46de7182d7d6bba10f72a3d7317c55653cf893b
WallyS02 <sebkutny@gmail.com>
2023-11-15 12:11:02 +0100

    Added proxy.conf.json and validation

--- magjob-frontend/src/app/login/view/login.component.ts
+++ magjob-frontend/src/app/login/view/login.component.ts
@@ -1,7 +1,7 @@
 import { LoginService } from '../service/login.service';
 import { Component } from '@angular/core';
 import { Login } from '../model/login';
-import { FormControl, FormGroup } from '@angular/forms';
+import {FormControl, FormGroup, Validators} from '@angular/forms';
 
 
 @Component({
@@ -12,24 +12,37 @@ import { FormControl, FormGroup } from '@angular/forms';
 export class LoginComponent {
 
   loginForm = new FormGroup({
-    email: new FormControl(''),
-    password: new FormControl('')
+    email: new FormControl(
+      '',
+      [
+        Validators.email,
+        Validators.required
+      ]),
+    password: new FormControl(
+      '',
+      [
+        Validators.pattern('^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d)(?=.*[@$!%*?&])[A-Za-z\\d@$!%*?&]{5,}$'),
+        Validators.required
+      ])
   });
+
   loginModel: Login = new Login('', ''); // Initialize with empty values
 
   constructor(private loginService: LoginService) {}
 
     submitApplication() {
-      // Call the login service to verify the user's credentials
-      this.loginService.login(this.loginModel).subscribe(
-        (response) => {
-          console.log('Login successful:', response);
-          // Add any additional logic after a successful login
-        },
-        (error) => {
-          console.error('Login failed:', error);
-          // Handle login failure (show error message, etc.)
-        }
-      );
+      if (!(this.loginForm.invalid && (this.loginForm.dirty || this.loginForm.touched)) && this.loginModel.email && this.loginModel.password) {
+        // Call the login service to verify the user's credentials
+        this.loginService.login(this.loginModel).subscribe(
+          (response) => {
+            console.log('Login successful:', response);
+            // Add any additional logic after a successful login
+          },
+          (error) => {
+            console.error('Login failed:', error);
+            // Handle login failure (show error message, etc.)
+          }
+        );
+      }
   }
 }
