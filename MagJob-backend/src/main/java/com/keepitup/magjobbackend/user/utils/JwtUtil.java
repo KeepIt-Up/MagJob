4c0fe9c95a4ea8334f28244439bf31712b1d4cf2
WallyS02 <sebkutny@gmail.com>
2023-11-18 18:04:52 +0100

    Added random secret key generation

--- MagJob-backend/src/main/java/com/keepitup/magjobbackend/user/utils/JwtUtil.java
+++ MagJob-backend/src/main/java/com/keepitup/magjobbackend/user/utils/JwtUtil.java
@@ -9,6 +9,8 @@ import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Component;
 
 import java.security.Key;
+import java.security.SecureRandom;
+import java.util.Base64;
 import java.util.Date;
 import java.util.HashMap;
 import java.util.Map;
@@ -17,7 +19,14 @@ import java.util.function.Function;
 @Component
 public class JwtUtil {
 
-    public static final String SECRET = "5367566B59703373367639792F423F4528482B4D6251655468576D5A71347437";
+    private static final String SECRET = generateSecretKey();
+
+    private static String generateSecretKey() {
+        SecureRandom secureRandom = new SecureRandom();
+        byte[] keyBytes = new byte[32];
+        secureRandom.nextBytes(keyBytes);
+        return Base64.getEncoder().encodeToString(keyBytes);
+    }
 
     public String extractUsername(String token) {
         return extractClaim(token, Claims::getSubject);
