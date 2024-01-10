e3342335085e3c5e538800fecd8b2ba6c3cf5924
kmonieczny <102854498+kmonieczny@users.noreply.github.com>
2024-01-10 14:28:24 +0100

    Added missing HttpComponentsClientHttpRequestFactory import

--- magjob-backend/src/main/java/com/keepitup/magjobbackend/MagJobBackendApplication.java
+++ magjob-backend/src/main/java/com/keepitup/magjobbackend/MagJobBackendApplication.java
@@ -5,6 +5,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.context.annotation.Bean;
+import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
 import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
