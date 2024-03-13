e6c00d3257cf973e5af8893eff66d0f03c99ad3f
Sebastian Kutny <97989092+WallyS02@users.noreply.github.com>
2024-03-13 17:14:38 +0100

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -16,25 +16,18 @@ cp .env MagJob
 cd MagJob
 
 docker run -d \
-  --name=keycloak-auxiliary \
-  -v keycloak:/keycloak \
-  -v ./keycloak/certs:/keycloak-auxiliary \
-  busybox cp -r /keycloak-auxiliary/* /keycloak
-
-docker run -d \
   --name=prometheus-auxiliary \
-  -v prometheus:/prometheus \
   -v ./monitoring/prometheus:/prometheus-auxiliary \
-  busybox cp -r /prometheus-auxiliary/* /prometheus
+  --mount type=volume,source=prometheus,target=/prometheus-auxiliary,volume-driver=vieux/sshfs,volume-opt=sshcmd=${STORAGE_USER}@${STORAGE_IP}:/home/ec2-user/volume/prometheus \
+  busybox cp -r /prometheus-auxiliary/* prometheus
 
 docker run -d \
   --name=grafana-auxiliary \
-  -v grafana:/grafana \
   -v ./monitoring/grafana/provisioning/datasources:/grafana-auxiliary \
-  busybox cp -r /grafana-auxiliary/* /grafana
+  --mount type=volume,source=grafana,target=/grafana-auxiliary,volume-driver=vieux/sshfs,volume-opt=sshcmd=${STORAGE_USER}@${STORAGE_IP}:/home/ec2-user/volume/grafana \
+  busybox cp -r /grafana-auxiliary/* grafana
 
 $(cat .env | sed 's/^/export /') && docker stack deploy -c docker-compose.yml $STACK_NAME --with-registry-auth
 
-docker container rm keycloak-auxiliary
 docker container rm prometheus-auxiliary
 docker container rm grafana-auxiliary
